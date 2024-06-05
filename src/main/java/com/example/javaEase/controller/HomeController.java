d756adfbb5204dec475e815ceb3e2a76f61473af
Pranav Tripathi <pranav.tripathi@nagarro.com>
2024-06-05 11:19:17 +0000

    Updated HomeController.java

--- src/main/java/com/example/javaEase/controller/HomeController.java
+++ src/main/java/com/example/javaEase/controller/HomeController.java
@@ -2,7 +2,7 @@ package com.example.javaEase.controller;
 
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
-import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
